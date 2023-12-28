d77afb01a551bcf3b677d3a000fcc5e42e74e6db
BrayanAdair <brayanadairmontano083@gmail.com>
2023-12-28 14:01:31 -0600

    Promociones Completo Version 1

--- SIPOH/Promociones.aspx.cs
+++ SIPOH/Promociones.aspx.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Text;
+using static SIPOH.Views.InicialAcusatorio;
 
 namespace SIPOH
 {
@@ -40,7 +42,14 @@ namespace SIPOH
             if (!IsPostBack)
             {
                 tituloTablaPromociones.Visible = false;
+                tituloDetallesCausa.Visible = false;
+                TablasAnexos.Visible = false;
+                insertarPromoventeAnexos.Style.Add("display", "none");
+                BotonGuardarDiv.Style.Add("display", "none");
                 CargarJuzgados();
+                CargarAnexos();
+                OtroAnexo.Disabled = true;
+                TablasAnexos.Visible = false;
             }
         }
         //
@@ -50,13 +59,12 @@ namespace SIPOH
             using (SqlConnection con = new SqlConnection(connectionString))
             {
                 con.Open();
-                int idDistrito = 11;
+                int circuito = Convert.ToInt32(HttpContext.Current.Session["IdCircuito"]);
 
-                using (SqlCommand cmd = new SqlCommand("Ejecucion_Cat_Juzgados", con))
+                using (SqlCommand cmd = new SqlCommand("Ejecucion_Cat_JuzgadosPorCircuitoE", con))
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@IdDistrito", idDistrito);
-
+                    cmd.Parameters.AddWithValue("@idCircuito", circuito);
                     SqlDataReader dr = cmd.ExecuteReader();
                     while (dr.Read())
                     {
@@ -78,6 +86,7 @@ namespace SIPOH
                 string mensajeError = "Por favor, selecciona un juzgado válido.";
                 ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "mostrarToastError", $"toastError('{mensajeError}');", true);
                 return;
+
             }
 
             string connectionString = ConfigurationManager.ConnectionStrings["SIPOHDB"].ConnectionString;
@@ -108,29 +117,353 @@ namespace SIPOH
                 tituloTablaPromociones.Visible = true;
                 GridViewPromociones.DataSource = dt;
                 GridViewPromociones.DataBind();
+                tituloDetallesCausa.Visible = false;
+                detallesCausa.InnerHtml = "";
+                insertarPromoventeAnexos.Style.Add("display", "none");
                 string mensajeExito = "Se encontraron resultados de tu consulta de promociones.";
                 ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "mostrarToastExito", $"mostrarToast('{mensajeExito}');", true);
             }
             else
             {
                 tituloTablaPromociones.Visible = false;
-               // tituloDetalles4.Visible = false;
+                tituloDetallesCausa.Visible = false;
                 string mensajeNoDatos = "No se encontro resultado de la busqueda.";
                 ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "mostrarToastNoDatos", $"toastError('{mensajeNoDatos}');", true);
+                insertarPromoventeAnexos.Style.Add("display", "none");
             }
         }
 
         protected void btnLimpiarPromocion_Click(object sender, EventArgs e)
         {
             tituloTablaPromociones.Visible = false;
-            //tituloDetalles5.Visible = false;
+            tituloDetallesCausa.Visible = false;
             GridViewPromociones.DataSource = null;
             GridViewPromociones.DataBind();
-            //detallesConsulta5.InnerHtml = "";
+            detallesCausa.InnerHtml = "";
+            insertarPromoventeAnexos.Style.Add("display", "none");
             string mensaje = "Se ha limpiado la busqueda y sus campos, puedes buscar de nuevo si lo deseas";
             string script = $"toastWarning('{mensaje}');";
             ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "mostrartoastWarning", script, true);
         }
+        protected void VerDetalles(int idAsunto)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["SIPOHDB"].ConnectionString;
+            StringBuilder htmlTable = new StringBuilder();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("Ejecucion_ModuloConsultasDetalle", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@IdAsunto", idAsunto);
+
+                    con.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        htmlTable.Append("<table class='table table-sm table-striped table-hover mb-0'>");
+                        htmlTable.Append("<thead>");
+                        htmlTable.Append("<tr class='text-center bg-primary text-white'>");
+                        htmlTable.Append("<th class='bg-success text-white'>Causa</th>");
+                        htmlTable.Append("<th class='bg-success text-white'>Juzgado</th>");
+                        htmlTable.Append("<th class='bg-success text-white'>Ofendidos</th>");
+                        htmlTable.Append("<th class='bg-success text-white'>Inculpados</th>");
+                        htmlTable.Append("<th class='bg-success text-white'>Delitos</th>");
+                        htmlTable.Append("</tr>");
+                        htmlTable.Append("</thead>");
+                        htmlTable.Append("<tbody>");
+
+                        if (dr.HasRows)
+                        {
+                            tituloTablaPromociones.Visible = true;
+                            tituloDetallesCausa.Visible = true;
+                            while (dr.Read())
+                            {
+                                htmlTable.Append("<tr>");
+                                htmlTable.Append($"<td class='text-dark'>{dr["Numero"]}</td>");
+                                htmlTable.Append($"<td class='text-secondary'>{dr["Juzgado"]}</td>");
+                                htmlTable.Append($"<td class='text-secondary'>{dr["Ofendidos"]}</td>");
+                                htmlTable.Append($"<td class='text-secondary'>{dr["Inculpados"]}</td>");
+                                htmlTable.Append($"<td class='text-secondary'>{dr["Delitos"]}</td>");
+                                htmlTable.Append("</tr>");
+                            }
+                        }
+                        else
+                        {
+                            htmlTable.Append("<tr><td colspan='2'>No se encontraron detalles.</td></tr>");
+                        }
+
+                        htmlTable.Append("</tbody>");
+                        htmlTable.Append("</table>");
+                    }
+                }
+            }
+
+            detallesCausa.InnerHtml = htmlTable.ToString();
+            insertarPromoventeAnexos.Style.Add("display", "block");
+        }
+        protected void GridViewPromociones_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "VerDetalles")
+            {
+                int idAsunto = Convert.ToInt32(e.CommandArgument);
+                VerDetalles(idAsunto);
+            }
+        }
+        //anexos
+        private void CargarAnexos()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["SIPOHDB"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM P_EjecucionCatAnexos WHERE Tipo = 'B' OR Descripcion = 'OTRO' ORDER BY CASE WHEN Descripcion = 'OTRO' THEN 1 ELSE 0 END, Descripcion", con))
+                {
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        ListItem listItem = new ListItem();
+                        listItem.Text = dr["Descripcion"].ToString();
+                        listItem.Value = dr["Descripcion"].ToString();
+                        CatAnexosDD.Items.Add(listItem);
+                    }
+                }
+            }
+        }
+        protected void CatAnexosDD_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OtroAnexo.Disabled = CatAnexosDD.SelectedValue != "OTRO";
+        }
+        protected void AgregarATabla(object sender, EventArgs e)
+        {
+            string anexo = CatAnexosDD.SelectedItem.Text;
+            string valorAnexo = CatAnexosDD.SelectedItem.Value; // Obtiene el valor del item seleccionado
+
+            if (anexo == "OTRO")
+            {
+                anexo = OtroAnexo.Value;
+            }
+            string cantidad = CantidadInput.Value;
+
+            List<Sala> salas = ViewState["anexos"] as List<Sala> ?? new List<Sala>();
+
+            // Verificar si la opción "Seleccionar" fue elegida
+            if (valorAnexo.Equals("Seleccionar", StringComparison.OrdinalIgnoreCase))
+            {
+                // Manejar el caso de la opción "Seleccionar"
+                MostrarMensajeToast("Debes seleccionar una opción válida");
+                TablasAnexos.Visible = false;
+                return; // Finaliza la ejecución de la función
+            }
+
+            // Verificar si los campos no están vacíos
+            if (!string.IsNullOrWhiteSpace(anexo) && !string.IsNullOrWhiteSpace(cantidad))
+            {
+                var salaExistente = salas.FirstOrDefault(s => s.NombreSala.Equals(anexo, StringComparison.OrdinalIgnoreCase));
+                if (salaExistente != null)
+                {
+                    // Actualizar la cantidad del anexo existente
+                    salaExistente.NumeroToca = cantidad;
+                }
+                else
+                {
+                    // Agregar un nuevo anexo
+                    salas.Add(new Sala { NombreSala = anexo, NumeroToca = cantidad });
+                }
+
+                ViewState["anexos"] = salas;
+                tablaDatos.DataSource = salas;
+                tablaDatos.DataBind();
+                TablasAnexos.Visible = true;
+                BotonGuardarDiv.Style.Add("display", "block");
+            }
+            else
+            {
+                // Manejar el caso de campos vacíos
+                MostrarMensajeToast("No puedes dejar campos vacíos");
+                TablasAnexos.Visible = false;
+            }
+        }
+
+        private void MostrarMensajeToast(string mensaje)
+        {
+            string script = $"toastError('{mensaje}');";
+            ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "mostrarToastScript", script, true);
+        }
+
+        protected void BorrarFila(object sender, GridViewDeleteEventArgs e)
+        {
+            List<Sala> salas = (List<Sala>)ViewState["anexos"];
+            salas.RemoveAt(e.RowIndex);
+            ViewState["anexos"] = salas;
+            tablaDatos.DataSource = salas;
+            tablaDatos.DataBind();
+        }
+        protected void tablaDatos_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                foreach (TableCell cell in e.Row.Cells)
+                {
+                    cell.CssClass = "text-secondary";
+                }
+            }
+        }
+
+        //guardar datos
+        protected void btnModalPromociones_Click(object sender, EventArgs e)
+        {
+            ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "abrirModal", "abrirModalGuardarDatos2();", true);
+        }
+
+        //AQUI EMPIEZA EL INSERT
+        protected void GridViewPromociones_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                HiddenField hiddenIdEjecucion = (HiddenField)e.Row.FindControl("HiddenIdEjecucion");
+                int idEjecucion = int.Parse(hiddenIdEjecucion.Value);
+
+                // Guardar el IdEjecucion en ViewState o en otro lugar para su uso posterior
+                ViewState["IdEjecucionSeleccionado"] = idEjecucion;
+            }
+        }
+
+        protected void btnGuardarPromocion_Click(object sender, EventArgs e)
+        {
+            int idEjecucion = 0; // Valor predeterminado, por ejemplo, 0
+
+            if (ViewState["IdEjecucionSeleccionado"] != null)
+            {
+                idEjecucion = (int)ViewState["IdEjecucionSeleccionado"];
+                // Continuar con el resto del código
+            }
+            else
+            {
+                string mensajeNoDatos = "No se pudo rastrear la procedencia del oficio verifica la consulta inicial.";
+                ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "mostrarToastNoDatos", $"toastError('{mensajeNoDatos}');", true);
+                return; // Asegúrate de salir del método si no hay un IdEjecucion válido
+            }
+            int idUser = 0;
+            if (HttpContext.Current.Session["IdUsuario"] != null && int.TryParse(HttpContext.Current.Session["IdUsuario"].ToString(), out idUser))
+            {
+                // idUser tiene ahora el valor de la sesión
+            }
+            else
+            {
+                string mensajeNoDatos = "No se encontro un usuario logeado.";
+                ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "mostrarToastNoDatos", $"toastError('{mensajeNoDatos}');", true);
+                return; // Salir del método si no hay un IdUser válido
+            }
+            string promovente = $"{inPromoventeNombre.Value} {inPromoventePaterno.Value} {inPromoventeMaterno.Value}";
+            DateTime fechaIngreso = DateTime.Now;
+            int totalAnexos = CalcularTotalAnexos();
+            string connectionString = ConfigurationManager.ConnectionStrings["SIPOHDB"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlTransaction transaction = conn.BeginTransaction();
+
+                try
+                {
+                    int idEjecucionPosterior = InsertarEnEjecucionPosterior(conn, transaction, idEjecucion, promovente, fechaIngreso, totalAnexos, idUser);
+
+                    // Insertar anexos utilizando el idEjecucionPosterior obtenido
+                    List<Sala> anexos = ViewState["anexos"] as List<Sala>;
+                    if (anexos != null)
+                    {
+                        foreach (Sala anexo in anexos)
+                        {
+                            string nombreAnexo = anexo.NombreSala;
+                            int cantidad = Convert.ToInt32(anexo.NumeroToca);
+                            InsertarDatosAnexos(conn, transaction, idEjecucion, idEjecucionPosterior, nombreAnexo, cantidad);
+
+                        }
+                    }
+
+                    transaction.Commit();
+                     ScriptManager.RegisterStartupScript(
+                        this.UpdatePanelPromociones,
+                        this.UpdatePanelPromociones.GetType(),
+                        "CerrarModalGuardarDatos2",
+                        "CerrarModalGuardarDatos2();",
+                        true
+                    );
+                    string mensajeExito = "¡Se guardaron los datos de la promocion exitoamente!.";
+                    ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "mostrarToastExito", $"mostrarToast('{mensajeExito}');", true);
+                    string scriptRecarga = "setTimeout(function(){ window.location.href = window.location.href; }, 5000);";
+                    ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "recargaPaginaScript", scriptRecarga, true);
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    ScriptManager.RegisterStartupScript(
+                        this.UpdatePanelPromociones,
+                        this.UpdatePanelPromociones.GetType(),
+                        "CerrarModalGuardarDatos2",
+                        "CerrarModalGuardarDatos2();",
+                        true
+                    );
+                    string mensajeError = "Ocurrió un error al procesar su solicitud.";
+                    ScriptManager.RegisterStartupScript(this.Page, this.Page.GetType(), "mostrarToastError", $"toastError('{mensajeError}');", true);
+                }
+                finally
+                {
+                    if (conn.State == System.Data.ConnectionState.Open)
+                        conn.Close();
+                }
+            }
+        }
+
+        private int CalcularTotalAnexos()
+        {
+            // Código para sumar las cantidades de los anexos de la tablaDatos
+            int total = 0;
+            // Suponiendo que tienes una lista de objetos Sala que representa los anexos
+            List<Sala> anexos = ViewState["anexos"] as List<Sala>;
+            if (anexos != null)
+            {
+                total = anexos.Sum(a => Convert.ToInt32(a.NumeroToca));
+            }
+            return total;
+        }
+
+        private int InsertarEnEjecucionPosterior(SqlConnection conn, SqlTransaction transaction, int idEjecucion, string promovente, DateTime fechaIngreso, int anexos, int idUser)
+        {
+            string query = @"INSERT INTO [SIPOH].[dbo].[P_EjecucionPosterior] (IdEjecucion, Promovente, FechaIngreso, Anexos, IdUser)
+                     VALUES (@IdEjecucion, @Promovente, @FechaIngreso, @Anexos, @IdUser);
+                     SELECT CAST(scope_identity() AS int);"; // Modificado para devolver el ID generado
+            using (SqlCommand cmd = new SqlCommand(query, conn, transaction))
+            {
+                cmd.Parameters.AddWithValue("@IdEjecucion", idEjecucion);
+                cmd.Parameters.AddWithValue("@Promovente", promovente);
+                cmd.Parameters.AddWithValue("@FechaIngreso", fechaIngreso);
+                cmd.Parameters.AddWithValue("@Anexos", anexos);
+                cmd.Parameters.AddWithValue("@IdUser", idUser);
+                return (int)cmd.ExecuteScalar(); // Devuelve el ID generado
+            }
+        }
+
+        private void InsertarDatosAnexos(SqlConnection conn, SqlTransaction transaction, int idEjecucion, int idEjecucionPosterior, string nombreAnexo, int cantidad)
+        {
+            string query = @"
+    INSERT INTO [SIPOH].[dbo].[P_EjecucionAnexos]
+    (IdEjecucion, IdEjecucionPosterior, Detalle, Cantidad)
+    VALUES
+    (@IdEjecucion, @IdEjecucionPosterior, @Detalle, @Cantidad)";
+
+            using (SqlCommand cmd = new SqlCommand(query, conn, transaction))
+            {
+                cmd.Parameters.AddWithValue("@IdEjecucion", idEjecucion);
+                cmd.Parameters.AddWithValue("@IdEjecucionPosterior", idEjecucionPosterior);
+                cmd.Parameters.AddWithValue("@Detalle", nombreAnexo);
+                cmd.Parameters.AddWithValue("@Cantidad", cantidad);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
+
+
         //
     }
 }
--- SIPOH/Promociones.aspx.designer.cs
+++ SIPOH/Promociones.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace SIPOH
         protected global::System.Web.UI.UpdatePanel UpdatePanelPromociones;
 
         /// <summary>
+        /// Control btnGuardarAcusatorio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardarAcusatorio;
+
+        /// <summary>
         /// Control selectBusJuzgados.
         /// </summary>
         /// <remarks>
@@ -85,5 +94,131 @@ namespace SIPOH
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridViewPromociones;
+
+        /// <summary>
+        /// Control tituloDetallesCausa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label tituloDetallesCausa;
+
+        /// <summary>
+        /// Control detallesCausa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl detallesCausa;
+
+        /// <summary>
+        /// Control insertarPromoventeAnexos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl insertarPromoventeAnexos;
+
+        /// <summary>
+        /// Control inPromoventeNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText inPromoventeNombre;
+
+        /// <summary>
+        /// Control inPromoventePaterno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText inPromoventePaterno;
+
+        /// <summary>
+        /// Control inPromoventeMaterno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText inPromoventeMaterno;
+
+        /// <summary>
+        /// Control CatAnexosDD.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList CatAnexosDD;
+
+        /// <summary>
+        /// Control OtroAnexo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText OtroAnexo;
+
+        /// <summary>
+        /// Control CantidadInput.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputGenericControl CantidadInput;
+
+        /// <summary>
+        /// Control AgregarBtn.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button AgregarBtn;
+
+        /// <summary>
+        /// Control TablasAnexos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label TablasAnexos;
+
+        /// <summary>
+        /// Control tablaDatos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView tablaDatos;
+
+        /// <summary>
+        /// Control BotonGuardarDiv.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl BotonGuardarDiv;
+
+        /// <summary>
+        /// Control btnGuardarDatosModal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardarDatosModal;
     }
 }
